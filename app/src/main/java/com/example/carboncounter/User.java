0e24f97b00a4cd31f9308e7ec5ae8d9df26263a3
Brian Lin <pyrhex@gmail.com>
2021-02-13 15:22:53 -0800

    updated User.java

--- app/src/main/java/com/example/carboncounter/User.java
+++ app/src/main/java/com/example/carboncounter/User.java
@@ -6,7 +6,7 @@ public class User{
     float gas;
     float dairy;
     float water;
-    public User(float beef, float pork, float lamb, float, chicken, float gas, float dairy, float water){
+    public User(float beef, float pork, float lamb, float chicken, float gas, float dairy, float water){
         this.beef = beef;
         this.pork = pork;
         this.lamb = lamb;
@@ -19,9 +19,16 @@ public class User{
         return this.gas * 2.4;
     }
     public float calcMeat(){
-        return this.beef * 27 + this.pork * 12.1 + this.lamb * 39.2 + this.chicken * 6.9;
+        return (float) (this.beef * 27 + this.pork * 12.1 + this.lamb * 39.2 + this.chicken * 6.9);
     }
-
-
+    public float calcDairy(){
+        return (float) (this.dairy * 0.93);
+    }
+    public float calcWater(){
+        return (float) (this.water * 0.38);
+    }
+    public float calcTotal(){
+        return this.calcTransport() + this.calcDairy() + this.calcWater() + this.calcMeat();
+    }
 
 }
