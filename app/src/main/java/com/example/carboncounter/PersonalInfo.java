48b44e51a820a0f46014c96dd6cbcf6b6d1114d7
jaredmur-phy <murphyjared2@gmail.com>
2021-02-13 15:18:38 -0800

    Modified user instance

--- app/src/main/java/com/example/carboncounter/PersonalInfo.java
+++ app/src/main/java/com/example/carboncounter/PersonalInfo.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class PersonalInfo extends AppCompatActivity {
 
@@ -22,10 +26,17 @@ public class PersonalInfo extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //calculate, go to viewer
+                calculate();
                 Intent intent = new Intent(getApplicationContext(), CalculatorResults.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void calculate() {
+        EditText beef = (EditText) findViewById(R.id.enterBeef);
+        String beefResult = beef.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(), beefResult, LENGTH_SHORT);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/carboncounter/User.java
+++ app/src/main/java/com/example/carboncounter/User.java
@@ -1,24 +1,82 @@
+package com.example.carboncounter;
+
 public class User{
-    float beef;
-    float pork;
-    float lamb;
-    float chicken;
-    float gas;
-    float dairy;
-    float water;
-    public User(float beef, float gas, float dairy, float water){
-        this.beef = beef;
-        this.gas = gas;
-        this.dairy = dairy;
-        this.water = water;
+    float beef, pork, lamb, chicken, gas, dairy, water;
+
+    private static User instance;
+
+    private User() {
+
     }
+
+    public static User getInstance() {
+        if(instance == null) {
+            instance = new User();
+        }
+        return instance;
+    }
+
     public float calcTransport(){
-        return this.gas * 2.4;
+        return (float) (this.gas * 2.4);
     }
     public float calcMeat(){
-        return this.beef * 27 + this.pork * 12.1 + this.lamb * 39.2 + this.chicken * 6.9;
+        return (float) (this.beef * 27 + this.pork * 12.1 + this.lamb * 39.2 + this.chicken * 6.9);
     }
 
+    //Getters and Setters
+    public float getBeef() {
+        return beef;
+    }
 
+    public void setBeef(float beef) {
+        this.beef = beef;
+    }
 
+    public float getPork() {
+        return pork;
+    }
+
+    public void setPork(float pork) {
+        this.pork = pork;
+    }
+
+    public float getLamb() {
+        return lamb;
+    }
+
+    public void setLamb(float lamb) {
+        this.lamb = lamb;
+    }
+
+    public float getChicken() {
+        return chicken;
+    }
+
+    public void setChicken(float chicken) {
+        this.chicken = chicken;
+    }
+
+    public float getGas() {
+        return gas;
+    }
+
+    public void setGas(float gas) {
+        this.gas = gas;
+    }
+
+    public float getDairy() {
+        return dairy;
+    }
+
+    public void setDairy(float dairy) {
+        this.dairy = dairy;
+    }
+
+    public float getWater() {
+        return water;
+    }
+
+    public void setWater(float water) {
+        this.water = water;
+    }
 }
