dec575205795473354954ad787c8daca2d5fe95a
Brian Lin <pyrhex@gmail.com>
2021-02-13 15:09:45 -0800

    Added User object

---
+++ app/src/main/java/com/example/carboncounter/User.java
@@ -0,0 +1,24 @@
+public class User{
+    float beef;
+    float pork;
+    float lamb;
+    float chicken;
+    float gas;
+    float dairy;
+    float water;
+    public User(float beef, float gas, float dairy, float water){
+        this.beef = beef;
+        this.gas = gas;
+        this.dairy = dairy;
+        this.water = water;
+    }
+    public float calcTransport(){
+        return this.gas * 2.4;
+    }
+    public float calcMeat(){
+        return this.beef * 27 + this.pork * 12.1 + this.lamb * 39.2 + this.chicken * 6.9;
+    }
+
+
+
+}
